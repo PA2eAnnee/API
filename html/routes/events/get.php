3811edc2330717e11a862fa291e84a99de8fe50c
diabolize-andi <andrei-malicek@outlook.fr>
2023-07-04 20:30:47 +0200

    Update get.php

--- html/routes/events/get.php
+++ html/routes/events/get.php
@@ -3,18 +3,29 @@ require_once __DIR__ . "/../../libraries/body.php";
 require_once __DIR__ . "/../../libraries/response.php";
 require_once __DIR__ . "/../../entities/events/get-events.php";
 
-try {
 
-    $body = getBody();
-    $events = getEvent($body);
+if (authorization(0)){
 
-    echo jsonResponse(200, ["X-School" => "ESGI"], [
-        "success" => true,
-        "events" => $events
-    ]);
-} catch (Exception $exception) {
-    echo jsonResponse(500, [], [
+    try {
+
+        $body = getBody();
+        $events = getEvent($body);
+    
+        echo jsonResponse(200, ["X-School" => "ESGI"], [
+            "success" => true,
+            "events" => $events
+        ]);
+    } catch (Exception $exception) {
+        echo jsonResponse(500, [], [
+            "success" => false,
+            "error" => $exception->getMessage()
+        ]);
+    }
+    
+
+}else{
+    echo jsonResponse(400, [], [
         "success" => false,
-        "error" => $exception->getMessage()
+        "error" => "Vous n'avez pas les droit néccessaire pour effectuer cette action"
     ]);
 }
