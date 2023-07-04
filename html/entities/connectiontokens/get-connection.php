eeba7c3225322dcfba628adeda811497ec1138ce
diabolize-andi <andrei-malicek@outlook.fr>
2023-07-04 16:41:28 +0200

    Update get-connection.php

--- html/entities/connectiontokens/get-connection.php
+++ html/entities/connectiontokens/get-connection.php
@@ -4,30 +4,25 @@ function getConnection(string $token): ?array
     require_once __DIR__ . "/../../database/connection.php";
     require_once __DIR__ . "/../tokens/get-tokens.php";
 
-    $token = getToken(["token" => $token]);
+    $tokenData = getToken(["token" => $token]);
 
-    if (empty($token)) {
-        return ["success" => false, "error" => "Token inexistant dans la BDD"];
+    if (empty($tokenData)) {
+        return ["id" => null, "error" => "Token inexistant dans la BDD"];
     }
 
     $current_time = time();
-    $token_creation_time = strtotime($token[0]['creation_time']);
+    $token_creation_time = strtotime($tokenData[0]['creation_time']);
     $time_elapsed = $current_time - $token_creation_time;
     $time_elapsed_hours = $time_elapsed / 3600;
 
-    
-   if ($time_elapsed_hours < 24) {
-    return ["success" => true];
-   }else {
-    $databaseConnection = getDatabaseConnection();
-    $insertTokenQuery = $databaseConnection->prepare("DELETE FROM TOKENS WHERE token = :token;");
-    $insertTokenQuery->execute([
-        "token" => $token[0]['token'],
-    ]);
-    return ["success" => false, "error" => "Token expiré"];
-  }
-
-
+    if ($time_elapsed_hours < 24) {
+        return ["id" => $tokenData[0]['id']];
+    } else {
+        $databaseConnection = getDatabaseConnection();
+        $deleteTokenQuery = $databaseConnection->prepare("DELETE FROM TOKENS WHERE token = :token;");
+        $deleteTokenQuery->execute([
+            "token" => $tokenData[0]['token'],
+        ]);
+        return ["id" => null, "error" => "Token expiré"];
+    }
 }
-
-
