f8d72b8a7dcc4201e38d40b3cdfe62e4a64a360c
diabolize-andi <andrei-malicek@outlook.fr>
2023-07-04 16:57:07 +0200

    Update get-connection.php

--- html/entities/connectiontokens/get-connection.php
+++ html/entities/connectiontokens/get-connection.php
@@ -1,35 +1,31 @@
 <?php
-function getConnection(string $token): ?array
+function getConnection(string $token): array
 {
     require_once __DIR__ . "/../../database/connection.php";
     require_once __DIR__ . "/../tokens/get-tokens.php";
 
-    $token = getToken(["token" => $token]);
+    $tokenData = getToken(["token" => $token]);
 
-    if (empty($token)) {
-        $databaseConnection = getDatabaseConnection();
-        $insertTokenQuery = $databaseConnection->prepare("SELECT user_id FROM TOKENS WHERE token = :token;");
-        $insertTokenQuery->execute([
-            "token" => $token[0]['token'],
-        ]);
+    if (empty($tokenData) || !isset($tokenData[0]['token'])) {
         return ["success" => false, "user_id" => null, "error" => "Token inexistant dans la BDD"];
     }
 
     $current_time = time();
-    $token_creation_time = strtotime($token[0]['creation_time']);
+    $token_creation_time = strtotime($tokenData[0]['creation_time']);
     $time_elapsed = $current_time - $token_creation_time;
     $time_elapsed_hours = $time_elapsed / 3600;
 
     
     if ($time_elapsed_hours < 24) {
-        return ["success" => true, "user_id" => $token[0]['user_id']];
+        return ["success" => true, "user_id" => $tokenData[0]['user_id']];
     } else {
         $databaseConnection = getDatabaseConnection();
         $insertTokenQuery = $databaseConnection->prepare("DELETE FROM TOKENS WHERE token = :token;");
         $insertTokenQuery->execute([
-            "token" => $token[0]['token'],
+            "token" => $tokenData[0]['token'],
         ]);
         return ["success" => false, "user_id" => null, "error" => "Token expiré"];
     }
 }
+
 
