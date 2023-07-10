0b420425247c18e3c8e4f6e1139a7180b19fefca
diabolize-andi <andrei-malicek@outlook.fr>
2023-07-10 05:30:57 +0200

    Update get-participate_lessons.php

--- html/entities/participate_lessons/get-participate_lessons.php
+++ html/entities/participate_lessons/get-participate_lessons.php
@@ -9,7 +9,7 @@ function getParticipate_lessons(?array $columns = null): array
 
     require_once __DIR__ . "/../../database/connection.php";
 
-    $authorizedColumns = ["user_id", "lesson_id", "recipeid", "last_consultation"];
+    $authorizedColumns = ["user_id", "lesson_id", "recipe_id", "last_consultation"];
 
     $where = [];
     $sanitizedColumns = [];
