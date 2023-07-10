3fe2cd575962a7e7dc909b5d76c777bb0458a6b2
diabolize-andi <andrei-malicek@outlook.fr>
2023-07-10 06:35:53 +0200

    Update post.php

--- html/routes/picture/post.php
+++ html/routes/picture/post.php
@@ -4,49 +4,27 @@
 if (isset($_FILES['photo'])) {
     // Vérifie si le dossier de destination existe, sinon le crée
     $uploadDir = 'pictures/';
-    echo($uploadDir);
     if (!is_dir($uploadDir)) {
         mkdir($uploadDir, 0777, true);
     }
 
-    // Récupère les informations sur le fichier
-    $fileInfo = getimagesize($_FILES['photo']['tmp_name']);
-    if ($fileInfo !== false) {
-        // Vérifie si le fichier est une image
-        $mime = $fileInfo['mime'];
-        $allowedMimeTypes = ['image/jpeg', 'image/png', 'image/gif'];
-        if (in_array($mime, $allowedMimeTypes)) {
-            // Génère un nom de fichier unique pour éviter les doublons
-            $fileName = uniqid() . '_' . $_FILES['photo']['name'];
-            $filePath = $uploadDir . $fileName;
+    // Génère un nom de fichier unique pour éviter les doublons
+    $fileName = uniqid() . '_' . $_FILES['photo']['name'];
+    $filePath = $uploadDir . $fileName;
 
-            // Déplace le fichier vers le dossier de destination
-            if (move_uploaded_file($_FILES['photo']['tmp_name'], $filePath)) {
-                // Le fichier a été téléchargé avec succès
-                $response = [
-                    'status' => 'success',
-                    'message' => 'Photo uploaded successfully.',
-                    'file_path' => $filePath
-                ];
-            } else {
-                // Une erreur s'est produite lors du téléchargement du fichier
-                $response = [
-                    'status' => 'error',
-                    'message' => 'Failed to upload photo.'
-                ];
-            }
-        } else {
-            // Le fichier n'est pas une image valide
-            $response = [
-                'status' => 'error',
-                'message' => 'Invalid file format. Only JPEG, PNG, and GIF images are allowed.'
-            ];
-        }
+    // Déplace le fichier vers le dossier de destination
+    if (move_uploaded_file($_FILES['photo']['tmp_name'], $filePath)) {
+        // Le fichier a été téléchargé avec succès
+        $response = [
+            'status' => 'success',
+            'message' => 'Photo uploaded successfully.',
+            'file_path' => $filePath
+        ];
     } else {
-        // Impossible de récupérer les informations sur le fichier
+        // Une erreur s'est produite lors du téléchargement du fichier
         $response = [
             'status' => 'error',
-            'message' => 'Failed to retrieve file information.'
+            'message' => 'Failed to upload photo.'
         ];
     }
 } else {
