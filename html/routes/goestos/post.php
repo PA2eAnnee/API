6a1512cd937da1ce4e23df09e279575f1ecdb23b
diabolize-andi <andrei-malicek@outlook.fr>
2023-07-10 02:28:31 +0200

    Update post.php

--- html/routes/goestos/post.php
+++ html/routes/goestos/post.php
@@ -7,25 +7,33 @@ require_once __DIR__ . "/../../libraries/authorization.php";
 require_once __DIR__ . "/../../entities/tokens/get-tokens.php";
 require_once __DIR__ . "/../../libraries/get-bearer-token.php";
 
-try {
-    $body = getBody();
+$body = getBody();
 
-    if (getToken(["token" => getBearerToken()])[0]["user_id"] == $body["id_user"] || authorization(2)) {
-        createGoesto($body["id_user"], $body["id_event"]);
+// Check if the required keys exist in the $body array
+if (isset($body["id_user"]) && isset($body["id_event"])) {
+    try {
+        if (getToken(["token" => getBearerToken()])[0]["user_id"] == $body["id_user"] || authorization(2)) {
+            createGoesto($body["id_user"], $body["id_event"]);
 
-        echo jsonResponse(200, [], [
-            "success" => true,
-            "message" => "Goesto ajouté"
-        ]);
-    } else {
-        echo jsonResponse(400, [], [
+            echo jsonResponse(200, [], [
+                "success" => true,
+                "message" => "Goesto ajouté"
+            ]);
+        } else {
+            echo jsonResponse(400, [], [
+                "success" => false,
+                "error" => "Vous n'avez pas les droits nécessaires pour effectuer cette action"
+            ]);
+        }
+    } catch (Exception $exception) {
+        echo jsonResponse(500, [], [
             "success" => false,
-            "error" => "Vous n'avez pas les droits nécessaires pour effectuer cette action"
+            "error" => "Une erreur s'est produite lors du traitement de la requête."
         ]);
     }
-} catch (Exception $exception) {
-    echo jsonResponse(500, [], [
+} else {
+    echo jsonResponse(400, [], [
         "success" => false,
-        "error" => "Une erreur s'est produite lors du traitement de la requête."
+        "error" => "Données manquantes dans le corps de la requête."
     ]);
 }
