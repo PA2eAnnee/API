8ff849342790afbaf8dfd71ec274f859ddc6634a
diabolize-andi <andrei-malicek@outlook.fr>
2023-07-10 09:13:04 +0200

    Update post.php

--- html/routes/picture/post.php
+++ html/routes/picture/post.php
@@ -1,17 +1,33 @@
+<?php
 
-    <?php
-    // Vérifie si une image a été soumise
-    if(isset($_FILES['image'])) {
-        $file = $_FILES['image'];
-        
-        // Vérifie s'il y a eu une erreur lors du téléchargement
-        if($file['error'] === UPLOAD_ERR_OK) {
-            echo "L'image a été téléchargée avec succès.";
-        } else {
-            echo "Une erreur s'est produite lors du téléchargement de l'image.";
-        }
+require_once __DIR__ . "/../../entities/users/create-user.php";
+require_once __DIR__ . "/../../entities/users/get-users.php";
+require_once __DIR__ . "/../../libraries/response.php";
+require_once __DIR__ . "/../../libraries/body.php";
+
+// Vérifie si une image a été envoyée
+if (isset($_FILES['photo'])) {
+    // Vérifie si le dossier de destination existe, sinon le crée
+    $uploadDir = __DIR__ . "/pictures/"; // Ajouter un slash avant "pictures"
+    if (!is_dir($uploadDir)) {
+        mkdir($uploadDir, 0777, true);
     }
-    ?>
+
+    // Génère un nom de fichier unique pour éviter les doublons
+    $fileName = uniqid() . '_' . $_FILES['photo']['name'];
+    $filePath = $uploadDir . $fileName;
+
+    echo($filePath);
+    echo($fileName);
+
+
+} else {
+    // Aucune image envoyée
+    $response = [
+        'status' => 'error',
+        'message' => 'No photo uploaded.'
+    ];
+}
 
 // Convertit la réponse en JSON
 header('Content-Type: application/json');
