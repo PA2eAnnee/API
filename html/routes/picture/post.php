8a3079252bf6053c903dda02597f5f3f5390dafa
diabolize-andi <andrei-malicek@outlook.fr>
2023-07-10 09:40:25 +0200

    Update post.php

--- html/routes/picture/post.php
+++ html/routes/picture/post.php
@@ -1,55 +1,56 @@
 <?php
 
-require_once __DIR__ . "/../../libraries/response.php";
 require_once __DIR__ . "/../../libraries/body.php";
+require_once __DIR__ . "/../../libraries/response.php";
+require_once __DIR__ . "/../../entities/recipes/create-recipe.php";
+require_once __DIR__ . "/../../libraries/authorization.php";
 
-// Vérifie si une image a été envoyée
-if (isset($_FILES['photo'])) {
-    // Vérifie si le dossier de destination existe, sinon le crée
-    $uploadDir = __DIR__ . "/pictures/"; // Ajouter un slash avant "pictures"
-    if (!is_dir($uploadDir)) {
-        if (!mkdir($uploadDir, 0777, true)) {
+if (authorization(1)) {
+    // Vérifie si une image a été envoyée
+    if (isset($_FILES['photo'])) {
+        // Vérifie si le dossier de destination existe, sinon le crée
+        $uploadDir = __DIR__ . "/../../img/"; // Ajouter un slash avant "pictures"
+        if (!is_dir($uploadDir)) {
+            if (!mkdir($uploadDir, 0777, true)) {
+                $response = [
+                    'status' => 'error',
+                    'message' => 'Failed to create upload directory.'
+                ];
+                http_response_code(500);
+                echo json_encode($response);
+                exit;
+            }
+        }
+
+        // Génère un nom de fichier unique pour éviter les doublons
+        $fileName = uniqid() . '_' . $_FILES['photo']['name'];
+        $filePath = $uploadDir . $fileName;
+
+        if (move_uploaded_file($_FILES['photo']['tmp_name'], $filePath)) {
+            // L'image a été téléchargée avec succès
+            $response = [
+                'status' => 'success',
+                'message' => 'Photo uploaded successfully.',
+                'file_name' => $fileName,
+                'file_path' => $filePath
+            ];
+            http_response_code(200);
+        } else {
+            // Échec du téléchargement de l'image
             $response = [
                 'status' => 'error',
-                'message' => 'Failed to create upload directory.'
+                'message' => 'Failed to upload the photo.'
             ];
             http_response_code(500);
-            echo json_encode($response);
-            exit;
         }
-    }
-
-    // Génère un nom de fichier unique pour éviter les doublons
-    $fileName = uniqid() . '_' . $_FILES['photo']['name'];
-    $filePath = $uploadDir . $fileName;
-
-    if (move_uploaded_file($_FILES['photo']['tmp_name'], $filePath)) {
-        // L'image a été téléchargée avec succès
-        $response = [
-            'status' => 'success',
-            'message' => 'Photo uploaded successfully.',
-            'file_name' => $fileName,
-            'file_path' => $filePath
-        ];
-        http_response_code(200);
     } else {
-        // Échec du téléchargement de l'image
+        // Aucune image envoyée
         $response = [
             'status' => 'error',
-            'message' => 'Failed to upload the photo.'
+            'message' => 'No photo uploaded.'
         ];
-        http_response_code(500);
+        http_response_code(400);
     }
-} else {
-    // Aucune image envoyée
-    $response = [
-        'status' => 'error',
-        'message' => 'No photo uploaded.'
-    ];
-    http_response_code(400);
-}
 
-// Convertit la réponse en JSON
-header('Content-Type: application/json');
-echo json_encode($response);
-?>
+    echo json_encode($response);
+}
