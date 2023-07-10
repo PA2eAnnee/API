d4a603311d534e295fa9cd9a6dc0b1ebafe7a0cf
diabolize-andi <andrei-malicek@outlook.fr>
2023-07-10 02:39:22 +0200

    Update get-bearer-token.php

--- html/libraries/get-bearer-token.php
+++ html/libraries/get-bearer-token.php
@@ -3,5 +3,6 @@
 function getBearerToken() {
     $headers = apache_request_headers();
     $token = isset($headers['Authorization']) ? $headers['Authorization'] : null;
+    $token = str_replace('Bearer ', '', $token);
     return $token;
 }
