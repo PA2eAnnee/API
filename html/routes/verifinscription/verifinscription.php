a0da9eb2bda57209937b21256b0ebce92b6d5a4a
diabolize-andi <andrei-malicek@outlook.fr>
2023-07-11 08:46:00 +0200

    Update verifinscription.php

--- html/routes/verifinscription/verifinscription.php
+++ html/routes/verifinscription/verifinscription.php
@@ -14,23 +14,38 @@
             animation: blink 1s infinite;
         }
     </style>
-    <meta http-equiv="refresh" content="5;url=page-de-connexion.html">
+    <?php
+    $redirectURL = 'page-de-connexion.html';
+    if ($finis === true) {
+        echo '<meta http-equiv="refresh" content="5;url=https://cookmaster.best">';
+        echo '<script>
+                setTimeout(function() {
+                    document.getElementById("redirect-message").textContent = "Redirection dans 2 secondes...";
+                    setTimeout(function() {
+                        document.getElementById("redirect-message").textContent = "Redirection dans 1 seconde...";
+                        setTimeout(function() {
+                            window.location.href = "https://cookmaster.best";
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+              </script>';
+        echo '<style>
+                h1::after {
+                    content: none;
+                }
+              </style>';
+        echo '<h1>Votre mail a été validé !</h1>';
+    } else {
+        echo '<meta http-equiv="refresh" content="5;url=' . $redirectURL . '">';
+    }
+    ?>
 </head>
 <body>
-    <h1>Validation de votre email en cours ...</h1>
-    <p>Vous serez redirigé(e) dans un instant vers la page de connexion.</p>
-    
     <?php
-    require_once __DIR__ . "/../../entities/tokens/get-tokens.php";
-
-
-    // Récupérer le token depuis l'URL
-    $token = $_GET['token'] ?? '';
-    $id= getToken(["token" => getBearerToken()])[0]["user_id"];
-    updateUser($id,["status" => "ACTIVE"]);
-
-
+    if ($finis !== true) {
+        echo '<h1>Validation de votre email en cours...</h1>';
+        echo '<p>Vous serez redirigé(e) dans un instant vers la page de connexion.</p>';
+    }
     ?>
-    
 </body>
 </html>
