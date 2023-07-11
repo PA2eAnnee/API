42bfa9815e3bdda885934bc665288ede64ba6d71
diabolize-andi <andrei-malicek@outlook.fr>
2023-07-11 04:38:04 +0200

    sfdfds

--- html/routes/articles/get.php
+++ html/routes/articles/get.php
@@ -5,6 +5,39 @@ require_once __DIR__ . "/../../entities/articles/get-articles.php";
 
 try {
 
+// Adresse e-mail de l'expéditeur
+$from = 'contact@cookmaster.best';
+
+// Adresse e-mail du destinataire
+$to = 'andrei-malicek@outlook.fr';
+
+// Sujet de l'e-mail
+$subject = 'Valider votre mot de passe !';
+
+// Charger le contenu du template
+$template = file_get_contents('/../../template/verifEmail.html');
+
+// Remplacer les variables du template avec les valeurs souhaitées
+$variables = array(
+    '{name}' => 'John Doe',
+    '{message}' => 'Ceci est le contenu de l\'e-mail.',
+);
+
+$body = strtr($template, $variables);
+
+// En-têtes de l'e-mail
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+$headers .= "From: $from" . "\r\n";
+
+// Envoyer l'e-mail
+if (mail($to, $subject, $body, $headers)) {
+    echo 'L\'e-mail a été envoyé avec succès.';
+} else {
+    echo 'Une erreur est survenue lors de l\'envoi de l\'e-mail.';
+}
+
+
     $body = getBody();
     $articles = getArticle($body);
 
